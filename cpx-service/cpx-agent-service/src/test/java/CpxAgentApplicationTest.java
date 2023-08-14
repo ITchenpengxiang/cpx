bc722b29b8566f982f25246948cd3da17b271ff3
陈鹏翔 <chenpengxiang@sinosoft.com.cn>
2023-08-14 15:28:59 +0800

    feat-第一次提交

---
+++ cpx-service/cpx-agent-service/src/test/java/CpxAgentApplicationTest.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author ：pengxiang chen
+ * @date ：Created in 2023-08-14
+ * @description ：
+ * @version: 1.0
+ */
+@SpringBootTest
+public class CpxAgentApplicationTest {
+
+
+}
